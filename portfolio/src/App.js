e6a93d9a426bc52438aecc4510e12d440b344ef6
jakeless <jacobtaylormoore@gmail.com>
2022-10-13 16:44:04 -0500

    had directory mislabeled in index.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import './App.css';
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
