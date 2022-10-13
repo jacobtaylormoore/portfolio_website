aa0d88dfe6717d01c243a255f741d0de4af066ab
jakeless <jacobtaylormoore@gmail.com>
2022-10-13 14:30:13 -0500

    ??

--- server/index.js
+++ server/index.js
@@ -19,6 +19,3 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
