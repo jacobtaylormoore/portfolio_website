414a226fad17a0e187d5230d9166dd12f062b10e
jakeless <jacobtaylormoore@gmail.com>
2022-10-17 19:03:18 -0500

    Adding pages. Submitting milestone #1

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,50 @@
-import React from "react";
+import React from 'react';
 import logo from "./logo.svg";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
+import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+//MUI
+import Typography from '@mui/material/Typography';
+
+
+// Pages
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import ExperiencePage from './pages/ExperiencePage';
+
+
+
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
+  //const [data, setData] = React.useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/experience" element={<ExperiencePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+// React.useEffect(() => {
+//   fetch("/")
+//     .then((res) => res.json())
+//     .then((data) => setData(data.message));
+// }, []);
+
+
 
 export default App;
---
+++ portfolio/src/pages/ContactPage.js
@@ -0,0 +1,69 @@
+// React
+import * as React from 'react';
+// Pages and components
+import Navigation from '../components/Navigation';
+
+// MUI
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+
+function ContactPage() {
+    const [value, setValue] = React.useState('Controlled');
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+    return (
+        <>
+            <Navigation />
+            <Grid container direction='column' alignItems='center' style={{ paddingTop: '10vh' }}>
+                <Box sx={{ width: '100%', maxWidth: 500 }}>
+                    <Typography variant='h2' >
+                        Contact Me
+                    </Typography>
+                </Box>
+            </Grid>
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 1, width: '24ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <div>
+                    <TextField
+                        id="outlined-textarea"
+                        label="Your Name"
+                        placeholder="John Smith"
+                        multiline
+                    />
+                    <TextField
+                        id="outlined-textarea"
+                        label="Your Email"
+                        placeholder="John@email.com"
+                        multiline
+                    />
+                </div>
+                <div>
+                    <TextField
+                        id="outlined-multiline-static"
+                        label="Your Message"
+                        placeholder="Hello..."
+                        fullWidth
+                        multiline
+                        rows={12}
+                    />
+                </div>
+            </Box>
+            <Button variant="contained">Submit</Button>
+        </>
+    );
+}
+
+export default ContactPage;
