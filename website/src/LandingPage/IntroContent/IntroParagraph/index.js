856f7179ba8e67127c79b9c131447c01c822fec7
jakeless <jacobtaylormoore@gmail.com>
2023-08-16 15:00:35 -0500

    new branch

--- website/src/LandingPage/IntroContent/IntroParagraph/index.js
+++ website/src/LandingPage/IntroContent/IntroParagraph/index.js
@@ -3,7 +3,9 @@ import './index.css';
 function IntroParagraph() {
     return (
         <div id="IntroParagraph">
-            <h1>Intro Paragraph</h1>
+            <div id="IntroHeader">
+                <h1>Intro Paragraph</h1>
+            </div>
         </div>
     );
 }
