8978d25e4a168da87c51fc901d996c2bcd987969
jakeless <jacobtaylormoore@gmail.com>
2023-08-16 12:16:15 -0500

    Changed location of Welcome Mesage

--- website/src/LandingPage/NavBar/index.js
+++ website/src/LandingPage/NavBar/index.js
@@ -5,10 +5,10 @@ function NavBar() {
     return (
         <section id="NavBar">
             <div id="TextContainer">
-                <NavText navLocation="#projectsContainer" text={"Projects"} />
-                <NavText navLocation="#experienceLayout" text={"Experience"} />
-                <NavText navLocation="#aboutPageContainer" text={"About Me"} />
-                <NavText navLocation="#contactPage" text={"Contact"} />
+                <NavText navLocation="#ProjectsContainer" text={"Projects"} />
+                <NavText navLocation="#Experience" text={"Experience"} />
+                <NavText navLocation="#AboutPage" text={"About Me"} />
+                <NavText navLocation="#ContactPage" text={"Contact"} />
             </div>
         </section>
     );
