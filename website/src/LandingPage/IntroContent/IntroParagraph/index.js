25f2ea735cac13de99633abc250df8686b277df5
jakeless <jacobtaylormoore@gmail.com>
2023-08-16 16:04:40 -0500

    added paragraph picture and border

--- website/src/LandingPage/IntroContent/IntroParagraph/index.js
+++ website/src/LandingPage/IntroContent/IntroParagraph/index.js
@@ -3,10 +3,40 @@ import './index.css';
 function IntroParagraph() {
     return (
         <div id="IntroParagraph">
-            <div id="IntroHeader">
-                <h1>Intro Paragraph</h1>
+            <div id="BorderBox" >
+                <div id="IntroHeader">
+                    <h1>Intro Paragraph</h1>
+                </div>
+                <div id="IntroTextBlock">
+                    <p>
+                        Amet qui pariatur sint fugiat sunt id aute tempor. Consectetur duis
+                        Lorem esse aliquip cillum Lorem nulla ea proident nisi laborum. Eiusmod
+                        velit et aliqua in. Qui voluptate ipsum sit tempor sit proident amet nulla
+                        laboris quis. Velit est duis nisi aliqua velit ipsum irure non tempor qui pariatur.
+                        Dolore labore anim deserunt reprehenderit qui mollit ad anim minim do cillum.
+                        Consectetur fugiat non elit incididunt Lorem deserunt esse nostrud aute sint qui.
+                        Amet qui pariatur sint fugiat sunt id aute tempor. Consectetur duis
+                        Lorem esse aliquip cillum Lorem nulla ea proident nisi laborum. Eiusmod
+                        velit et aliqua in. Qui voluptate ipsum sit tempor sit proident amet nulla
+                        laboris quis. Velit est duis nisi aliqua velit ipsum irure non tempor qui pariatur.
+                        Dolore labore anim deserunt reprehenderit qui mollit ad anim minim do cillum.
+                        Consectetur fugiat non elit incididunt Lorem deserunt esse nostrud aute sint qui.
+                        Amet qui pariatur sint fugiat sunt id aute tempor. Consectetur duis
+                        Lorem esse aliquip cillum Lorem nulla ea proident nisi laborum. Eiusmod
+                        velit et aliqua in. Qui voluptate ipsum sit tempor sit proident amet nulla
+                        laboris quis. Velit est duis nisi aliqua velit ipsum irure non tempor qui pariatur.
+                        Dolore labore anim deserunt reprehenderit qui mollit ad anim minim do cillum.
+                        Consectetur fugiat non elit incididunt Lorem deserunt esse nostrud aute sint qui.
+                        Amet qui pariatur sint fugiat sunt id aute tempor. Consectetur duis
+                        Lorem esse aliquip cillum Lorem nulla ea proident nisi laborum. Eiusmod
+                        velit et aliqua in. Qui voluptate ipsum sit tempor sit proident amet nulla
+                        laboris quis. Velit est duis nisi aliqua velit ipsum irure non tempor qui pariatur.
+                        Dolore labore anim deserunt reprehenderit qui mollit ad anim minim do cillum.
+                        Consectetur fugiat non elit incididunt Lorem deserunt esse nostrud aute sint qui.
+                    </p>
+                </div>
             </div>
-        </div>
+        </div >
     );
 }
 
--- website/src/LandingPage/IntroContent/PictureContainer/index.js
+++ website/src/LandingPage/IntroContent/PictureContainer/index.js
@@ -1,9 +1,17 @@
 import './index.css';
+import Image from '../../../images/headshot.jpg';
 
 function PictureContainer() {
     return (
         <div className='PictureContainer' id='PictureContainer'>
-            <h1>Pictures</h1>
+            <div id="PictureHeader">
+                <h1>Pictures</h1>
+            </div>
+            <div id="PictureWrap">
+                <div id="EmptySpace"></div>
+                <img id="Picture" src={Image} alt="headshot" />
+                <div id="EmptySpace"></div>
+            </div>
         </div>
     )
 }
