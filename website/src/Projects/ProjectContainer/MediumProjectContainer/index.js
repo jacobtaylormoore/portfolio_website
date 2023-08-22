ee2fd4cd45be73c41d3e3bc49c655b54e9dba4a7
Jacob Moore <mooreja2@oregonstate.edu>
2023-08-22 12:46:05 -0500

    small changes - adding new folders and text

---
+++ website/src/Projects/ProjectContainer/MediumProjectContainer/index.js
@@ -0,0 +1,12 @@
+import ProjectText from '../ProjectText';
+import './index.css';
+
+function MediumProjectContainer() {
+    return (
+        <div id="MediumProjectContainer">
+            <ProjectText />
+        </div>
+    );
+}
+
+export default MediumProjectContainer;
