fa8804b1dd11cd0de11d3cb71edf9509c4950670
jakeless <jacobtaylormoore@gmail.com>
2023-08-22 16:03:35 -0500

    -

---
+++ website/src/Projects/MediumProjectContainer/PicNameTechContainer/ProjectPic/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function ProjectPic() {
+    return (
+        <div id="ProjectPic">
+            <h3>BriefDescription</h3>
+        </div>
+    );
+}
+
+export default ProjectPic;
---
+++ website/src/Projects/MediumProjectContainer/PicNameTechContainer/ProjectTech/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function ProjectTech() {
+    return (
+        <div id="ProjectTech">
+            <h3>BriefDescription</h3>
+        </div>
+    );
+}
+
+export default ProjectTech;
