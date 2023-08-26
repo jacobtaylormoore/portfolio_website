55062f31cb9679926b46578178e4b64ed0583b21
jakeless <jacobtaylormoore@gmail.com>
2023-08-26 10:00:51 -0500

    About page setup

--- website/src/AboutPage/index.js
+++ website/src/AboutPage/index.js
@@ -1,10 +1,9 @@
-import DieExample from './DieExample';
+
 import './index.css';
 
 function AboutPage() {
     return (
         <div id='AboutPage'>
-            <DieExample />
         </div>
     );
 }
