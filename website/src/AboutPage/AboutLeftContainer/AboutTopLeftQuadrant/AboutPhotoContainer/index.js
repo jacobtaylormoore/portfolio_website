afa55510ad1355a440bc95a00c6ee9c282f39d19
jakeless <jacobtaylormoore@gmail.com>
2023-08-26 10:27:26 -0500

    File Structure

---
+++ website/src/AboutPage/AboutLeftContainer/AboutTopLeftQuadrant/AboutPhotoContainer/index.js
@@ -0,0 +1,12 @@
+
+import './index.css';
+
+function AboutPhotoContainer() {
+    return (
+        <div id='AboutPhotoContainer'>
+            <p>Photo</p>
+        </div>
+    );
+}
+
+export default AboutPhotoContainer;
---
+++ website/src/AboutPage/AboutRightContainer/AboutParagraphContainer/index.js
@@ -0,0 +1,12 @@
+
+import './index.css';
+
+function AboutParagraphContainer() {
+    return (
+        <div id='AboutParagraphContainer'>
+            <p>paragraph</p>
+        </div>
+    );
+}
+
+export default AboutParagraphContainer;
