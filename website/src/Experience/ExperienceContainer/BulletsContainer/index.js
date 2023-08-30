d42e08ebc82c7fb46341f5da4ce5bc5c5535c765
jakeless <jacobtaylormoore@gmail.com>
2023-08-30 12:52:10 -0500

    silly styles change

--- website/src/Experience/ExperienceContainer/BulletsContainer/index.js
+++ website/src/Experience/ExperienceContainer/BulletsContainer/index.js
@@ -1,10 +1,11 @@
 import './index.css';
 
 function BulletsContainer(props) {
+    const styles = { color: "orange" }
     return (
         <div id="BulletsContainer">
             <ul>
-                {props.bullets.map((bullet, i) => <li id={i}>{bullet}</li>)}
+                {props.bullets.map((bullet, i) => <li id={i} style={styles}>{bullet}</li>)}
             </ul>
         </div>
     );
