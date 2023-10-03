f207eb48f27506053af89a4afbb0ca06cfa418c2
jakeless <jacobtaylormoore@gmail.com>
2023-10-03 16:44:58 -0500

    style changes for experience box and added about me paragraph

--- website/src/AboutPage/AboutRightContainer/AboutParagraphContainer/index.js
+++ website/src/AboutPage/AboutRightContainer/AboutParagraphContainer/index.js
@@ -4,7 +4,18 @@ import './index.css';
 function AboutParagraphContainer() {
     return (
         <div id='AboutParagraphContainer'>
-            <p>paragraph</p>
+            <h1>
+                About Me
+            </h1>
+            <p>
+                I’ve always been very passionate. I try my hardest to grow as a person every day. I love being a part of a group with a shared sense of community and I love connecting people wherever I can. Some of my many interests include playing team sports (especially basketball), creating small, fun projects on my Raspberry Pi, reading (especially Terry Pratchett), finding new movies, bouldering and traveling. My wife and I are constantly working to experience as much life as we can while we can.
+            </p>
+
+            <p>
+                The people I love mean everything to me and I’ve been lucky enough to be surrounded by incredible people. My wife, Alayna, and I got married in December of 2021 after dating for about 8 years. She is my best friend and we do just about everything together to this day along with our two dogs, Leonard and Luka. I’ve also built strong relationships with my friends here in town and all over the country.</p>
+            <p>
+                I pride myself on being adaptable in uncomfortable situations and learning from the inevitable failures in life. I love collaborating with other people and being a part of exciting projects. But, whatever we’re working on, I always value the “Who” over the “What.”
+            </p>
         </div>
     );
 }
--- website/src/Experience/ExperienceContainer/BulletsContainer/index.js
+++ website/src/Experience/ExperienceContainer/BulletsContainer/index.js
@@ -1,11 +1,11 @@
 import './index.css';
 
 function BulletsContainer(props) {
-    const styles = { color: "orange" }
+
     return (
         <div id="BulletsContainer">
             <ul>
-                {props.bullets.map((bullet, i) => <li id={i} style={styles}>{bullet}</li>)}
+                {props.bullets.map((bullet, i) => <li id={i}>{bullet}</li>)}
             </ul>
         </div>
     );
