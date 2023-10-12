aef21726aac724b12e1077b0f455413525a171c8
jakeless <jacobtaylormoore@gmail.com>
2023-10-12 18:35:10 -0500

    Revert "contact page started"
    This reverts commit 46fbaaedebdb9bb685eb07d4343152cd9bf7fb67.

--- website/src/index.js
+++ website/src/index.js
@@ -4,15 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
