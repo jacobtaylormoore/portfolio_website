01abc4830f932ec557b1d27f6d594fa2b442c74b
jakeless <jacobtaylormoore@gmail.com>
2023-10-12 18:49:02 -0500

    Revert "changes in package. trying to get contact pgcomm"
    This reverts commit 2ce8a026195a92339626f4309c15bf33fe80fa4a.

--- website/src/index.js
+++ website/src/index.js
@@ -4,8 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+<<<<<<< HEAD
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+=======
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+>>>>>>> parent of 2ce8a02 (changes in package. trying to get contact pgcomm)
   <React.StrictMode>
     <App />
   </React.StrictMode>
